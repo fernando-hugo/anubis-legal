413d48b294f3e977e52e3fb381fc23f149ad68df
Fernando Hugo Ferreira <fernandohugoferreira@gmail.com>
2026-03-10 16:00:35 -0300

    fix: prisma.config.ts final

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,7 +1,9 @@
-// @ts-nocheck
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
   earlyAccess: true,
   schema: "prisma/schema.prisma",
+  migrate: {
+    url: process.env.DIRECT_URL ?? process.env.DATABASE_URL ?? "",
+  },
 });
