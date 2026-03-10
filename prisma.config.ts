e969a8a7d8591b5f992f55b64c9de3af7423b4e0
Fernando Hugo Ferreira <fernandohugoferreira@gmail.com>
2026-03-10 13:36:03 -0300

    fix: prisma config url para migrate deploy

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,21 +3,21 @@ import { defineConfig } from "prisma/config";
 import { PrismaNeon } from "@prisma/adapter-neon";
 import { neon } from "@neondatabase/serverless";
 
-const connectionString = process.env.DATABASE_URL!;
-
 export default defineConfig({
+  earlyAccess: true,
   schema: "prisma/schema.prisma",
 
   client: {
     adapter() {
-      const sql = neon(connectionString);
+      const sql = neon(process.env.DATABASE_URL!);
       return new PrismaNeon(sql);
     },
   },
 
   migrate: {
+    url: process.env.DIRECT_URL!,
     adapter() {
-      const sql = neon(connectionString);
+      const sql = neon(process.env.DIRECT_URL!);
       return new PrismaNeon(sql);
     },
   },
