dfbffeb60dc900d4c5cf36ecb0fb0eb32eb2806c
Fernando Hugo Ferreira <fernandohugoferreira@gmail.com>
2026-03-09 23:18:25 -0300

    Corrige Prisma para deploy Vercel

--- prisma.config.ts
+++ prisma.config.ts
@@ -7,10 +7,17 @@ const connectionString = process.env.DATABASE_URL!;
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
+
+  // Configuração do cliente Prisma com Neon
   client: {
     adapter() {
       const sql = neon(connectionString);
       return new PrismaNeon(sql);
     },
+  },
+
+  // Seed automático (opcional)
+  migrations: {
+    seed: "ts-node --compiler-options {\"module\":\"CommonJS\"} prisma/seed.ts",
   },
 });
