537cb10a736a68c9e114f59d3ab1d259126da296
Fernando Hugo Ferreira <fernandohugoferreira@gmail.com>
2026-03-09 23:08:13 -0300

    fix: refactor prisma config to match PrismaConfig type

--- prisma.config.ts
+++ prisma.config.ts
@@ -7,20 +7,10 @@ const connectionString = process.env.DATABASE_URL!;
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
-  migrate: {
-    url: connectionString,
-    adapter() {
-      const sql = neon(connectionString);
-      return new PrismaNeon(sql);
-    },
-  },
   client: {
     adapter() {
       const sql = neon(connectionString);
       return new PrismaNeon(sql);
     },
-  },
-  migrations: {
-    seed: "ts-node --compiler-options {\"module\":\"CommonJS\"} prisma/seed.ts",
   },
 });
